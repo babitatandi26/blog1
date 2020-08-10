32c9a45afe85476b6bf008ee301704490324981d
babitatandi26 <babitatandi266@gmail.com>
2020-08-10 17:05:38 +0530

    Updates

---
+++ precache-manifest.df1b8fe16c19f853ad2a499115706141.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cb008bf7829de0e765ea74443bac75",
+    "url": "/blog1/index.html"
+  },
+  {
+    "revision": "b74a535b06623d8816ff",
+    "url": "/blog1/static/css/2.5b7efe6d.chunk.css"
+  },
+  {
+    "revision": "12e937804f2cc80cfa01",
+    "url": "/blog1/static/css/main.6b4d4b3b.chunk.css"
+  },
+  {
+    "revision": "b74a535b06623d8816ff",
+    "url": "/blog1/static/js/2.2d8cfa8e.chunk.js"
+  },
+  {
+    "revision": "43714bcb155588ed2fb7f65075154cac",
+    "url": "/blog1/static/js/2.2d8cfa8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e937804f2cc80cfa01",
+    "url": "/blog1/static/js/main.fd5b46c8.chunk.js"
+  },
+  {
+    "revision": "3c1788c0b1e4932735c7",
+    "url": "/blog1/static/js/runtime-main.ad5ce4a4.js"
+  },
+  {
+    "revision": "ad3f365dc25aa72d3c3a6b9a0c274908",
+    "url": "/blog1/static/media/1.ad3f365d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog1/static/media/logo.5d5d9eef.svg"
+  }
+]);
